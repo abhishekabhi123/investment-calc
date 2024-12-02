e547438572dedc6481d0494434bcb2428b76c9ab
Abhishek P S <vadakkemadamabhishek@gmail.com>
2024-12-02 23:42:14 +0530

    userinput funcion added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function handleChange(inputIdentifer, newValue) {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [inputIdentifer]: +newValue,
+      };
+    });
+  }
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
+    </>
+  );
 }
 
-export default App
+export default App;
